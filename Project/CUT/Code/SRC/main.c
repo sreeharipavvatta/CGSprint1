d9b4a2e2076e2f7e080fe5a80d267718520f694e
Aman Bhaskar <amanbh2@gmail.com>
2022-09-07 17:42:37 +0530

    added main file

---
+++ Project/CUT/Code/SRC/main.c
@@ -0,0 +1,12 @@
+/*
+Filename: main.c
+Description: This is our main file program starts from here.
+*/
+#include <stdio.h>
+#include <pthread.h>
+#define SUCCESS 1
+#define ERROR 0
+int main(){
+
+    return SUCCESS;
+}
