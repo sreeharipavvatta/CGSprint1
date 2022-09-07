6995bd4eebd971d9c27a9c777d1d9dbed4a5d2ac
Aman Bhaskar <amanbh2@gmail.com>
2022-09-07 20:11:19 +0530

    Added prototype

--- Project/CUT/Code/SRC/main.c
+++ Project/CUT/Code/SRC/main.c
@@ -4,9 +4,14 @@ Description: This is our main file program starts from here.
 */
 #include <stdio.h>
 #include <pthread.h>
+#include "../Header/fun_head.h"
 #define SUCCESS 1
 #define ERROR 0
-int main(){
+int main(int argc, char *argv){
+
+    getDefect("../data/defect.txt");
+
+
 
     return SUCCESS;
 }
