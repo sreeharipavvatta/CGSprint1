fb94baa55593699e7d940b4c4fa14b9c9ed28f8d
ssvk053 <ssvk053@gmail.com>
2022-09-07 22:53:41 +0530

    Update getdefect.c
    check validity

--- Project/CUT/Code/SRC/getdefect.c
+++ Project/CUT/Code/SRC/getdefect.c
@@ -4,6 +4,7 @@ Description: Read all defects from input file.
 */
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
 #define SUCCESS 1
 #define ERROR 0
 #define MAXDEFECT 10
@@ -18,10 +19,31 @@ struct defect{
 
 };
 typedef struct defect defect;
-void checkValidity(char *str){
 
+//Defect No:Description:Module name:Functional area:Filed-on Date:Status:Type 
+void checkValidity(char *str){
+    int count=0;
+    while(1){
+        char *status;
+        char* token = strtok(str,":");
+        while(token != NULL){
+            count++;
+            if(count==6){
+                status = token;
+            }
+        }
+        if(count==7){
+          if(status=="open")
+           validDefect(defect_arr,str);
+        }
+        else{
+            invalidDefect(str);
+        }
+    }
+      
 }
 void validDefect(defect *defect_arr, char *str){
+     
 
 }
 void invalidDefect(char *str){
