e5417b8e86e0fd8cf694945fe945a564a2ca27b0
ssvk053 <ssvk053@gmail.com>
2022-09-08 12:00:43 +0530

    check validity  changed
    removed validdefect and tokenized in checkvalidity itself

--- Project/CUT/Code/SRC/getdefect.c
+++ Project/CUT/Code/SRC/getdefect.c
@@ -21,34 +21,52 @@ struct defect{
 typedef struct defect defect;
 
 //Defect No:Description:Module name:Functional area:Filed-on Date:Status:Type 
-void checkValidity(char *str){
+void checkValidity(defect *defect_arr,char *str){
     int count=0;
-    while(1){
-        char *status;
-        char* token = strtok(str,":");
+    char *s = (char*)calloc(sizeof(str),sizeof(char));
+    s = str;
+
+           char* token = strtok(s,":");
         while(token != NULL){
-            count++;
-            if(count==6){
-                status = token;
-            }
+             count++;
+             switch (count)
+             {
+             case 1 : defect_arr->defectID=(char*)calloc(sizeof(token),sizeof(char));
+                      defect_arr->defectID=token; break;
+             case 2 : defect_arr->description=(char*)calloc(sizeof(token),sizeof(char));
+                      defect_arr->description=token; break;
+             case 3 : defect_arr->moduleName=(char*)calloc(sizeof(token),sizeof(char));
+                      defect_arr->moduleName=token; break;
+             case 4 : defect_arr->functionalArea=(char*)calloc(sizeof(token),sizeof(char));
+                      defect_arr->functionalArea=token; break;
+             case 5 : defect_arr->data=(char*)calloc(sizeof(token),sizeof(char));
+                      defect_arr->data=token; break;
+             case 6 : defect_arr->status=(char*)calloc(sizeof(token),sizeof(char));
+                      defect_arr->status=token; break;
+             case 7 : defect_arr->type=(char*)calloc(sizeof(token),sizeof(char));
+                      defect_arr->type=token; break;
+             default:break;
+             }
+             token = strtok(NULL,":");
         }
+
         if(count==7){
-          if(status=="open")
+          if(defect_arr->status=="open")
            validDefect(defect_arr,str);
         }
         else{
-            invalidDefect(str);
+            invalidDefect(defect_arr->defectID,str);
         }
     }
       
 }
 void validDefect(defect *defect_arr, char *str){
      
+}
+void invalidDefect(char* defectID,char *str){
 
 }
-void invalidDefect(char *str){
 
-}
 void getDefect(char *file_loc){
     defect * defect_arr[MAXDEFECT];
 
